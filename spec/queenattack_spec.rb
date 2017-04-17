fde7c4cdbabfc4c2dae5505e91a5bcbe78cb71c9
Max Reichman and Peter Karellas <peter@>
2017-04-17 14:31:17 -0700

    final

--- spec/queenattack_spec.rb
+++ spec/queenattack_spec.rb
@@ -3,7 +3,7 @@ require('queen_attack?')
 
 describe('Array#queen_attack?') do
   it('is false if the coordinates are not horizontally, vertically, or diagonally in line with each other') do
-    expect([3,2].queen_attack?([1, 4])).to(eq(true))
+    expect([4,6].queen_attack?([5, 3])).to(eq(true))
   end
   it('is true if the coordinates are horizontally in line with each other') do
     expect([1,1].queen_attack?([1, 3])).to(eq(true))
