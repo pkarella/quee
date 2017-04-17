3999f97cc65c26d3a486b03954c9a21dab70dbbe
Max Reichman and Peter Karellas <max@>
2017-04-17 14:17:31 -0700

    Fix it.

--- lib/queen_attack?.rb
+++ lib/queen_attack?.rb
@@ -1,6 +1,8 @@
 class Array
   define_method(:queen_attack?) do |coordinates|
-    if self[0] + 1 == coordinates[0] && self[1] + 1 == coordinates[1] || self[0] - 1 == coordinates[0] && self[1] + 1 == coordinates[1] || self[0] + 1 === coordinates[0] && self[1] - 1 == coordinates[1] || self[0] == coordinates[0] || self[1] == coordinates[1]
+    if self[0].-(coordinates[0]) == self[1].-(coordinates[1]) || self[0].-(coordinates[1]) == self[1].-(coordinates[0])
+      true
+    elsif self[0] == coordinates[0] || self[1] == coordinates[1]
       true
     else
       false
