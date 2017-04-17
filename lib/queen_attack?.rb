5a304619206cfcedd59b9ea5df311c71dc2c2494
Max Reichman and Peter Karellas <peter@>
2017-04-17 11:34:56 -0700

    Set up the project.

---
+++ lib/queen_attack?.rb
@@ -0,0 +1,9 @@
+class Array
+  define_method(:queen_attack?) do |coordinates|
+    if self[0] + 1 == coordinates[0] && self[1] + 1 == coordinates[1] || self[0] == coordinates[0] || self[1] == coordinates[1]
+      true
+    else
+      false
+    end
+  end
+end
